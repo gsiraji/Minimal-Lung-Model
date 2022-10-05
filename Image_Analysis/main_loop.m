713942cab8673b71f9d69f4f3a3b0f7910732727
gsiraji <gessiraji@brandeis.edu>
2022-10-05 12:03:33 -0400

    Added comments to the code and a catch statement for reading images

--- Image_Analysis/main_loop.m
+++ Image_Analysis/main_loop.m
@@ -1,33 +1,50 @@
+% find the first empty row in the Excel sheet
 startingIndx = findEmptyRow('tissue_image_data.xlsx');
 %%
-for slideId = 1:10
-day = 1;
-slide = 27;
-% slideId = 3;
-processedImages = slideId+startingIndx;
-imageTitle = strcat(num2str(slide),'-',num2str(slideId));
-range1 = strcat('A',num2str(processedImages),':I',num2str(processedImages));
-im1 = imread(imageTitle+".tif");
-r2 = 0;
-r1= 34; %5um
-imEr = entropyFilter(r1, im1);
-% se = strel('disk', r,4);imEr = imerode(imEnt,se);
+% loop over all slide ID's and calculate stats
+for slide = 11
+    % loop over all images belonging to slide ID
+    for slideId = 1:2
+        % +1 the index
+        processedImages = slideId+startingIndx;
+        % find the corresponding image by title
+        imageTitle = strcat(num2str(slide),'-',num2str(slideId));
+        try
+            im1 = imread(imageTitle+".tif");
+        catch
+            warning('Read all existing files.')
+            break
+        end
+        % apply the entropy filter w structuring element of radius r1
+        r2 = 0;
+        r1= 34; %5um
+        imEr = entropyFilter(r1, im1);
+        % calculate the tissue fraction area (TFA)
+        area1 = sum(imEr,'all');
+        fraction = area1/numel(imEr) ;
+        % create the skeleton to calculate the length1 of the centerline
+        imSk = bwskel(imEr);
+        length1 = sum(imSk,'all');
+        % calculate the width from TFA and centerline length
+        width = area1/length1;
 
-imSk = bwskel(imEr);
-% imshowpair(im1,imEr,'montage')
-area1 = sum(imEr,'all');
-fraction = area1/numel(imEr) ;
-length1 = sum(imSk,'all');
-lengthOverArea = length1/area1;
-Table1 = table(day, slide,slideId,fraction,r1,r2,length1,area1,lengthOverArea);
-writetable(Table1, 'tissue_image_data.xlsx', 'Range',...
-    range1,'WriteVariableNames',0)
+        % % Save Data: Table and Binary Image  % %
+
+        % set the day, currently manually
+        % update to do: find the day from the lookup table
+        day = 7;
+        % set the correct range for inserting table in sheet
+        range1 = strcat('A',num2str(processedImages),':I',num2str(processedImages));
+        Table1 = table(day, slide,slideId,fraction,r1,r2,length1,area1,width);
+        writetable(Table1, 'tissue_image_data.xlsx', 'Range',...
+            range1,'WriteVariableNames',0)
+        % save the binary image (entropy filtered)
+        save(imageTitle+".mat","imEr")
+    end
 end
 
 
 function rowIndx = findEmptyRow(fileName)
-
 tempT = readtable(fileName);
 rowIndx = size(tempT,1);
-
 end
