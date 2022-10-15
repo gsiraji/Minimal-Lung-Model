fccaf04d35974dd5b721f23db168fc3f3ffccd33
srlubkin <113372563+srlubkin@users.noreply.github.com>
2022-10-15 17:00:17 -0400

    added calculation for Requivalent

--- Image_Analysis/Requiv.m
+++ Image_Analysis/Requiv.m
@@ -9,4 +9,16 @@ histogram(sqrt([areaCC.Area]/pi)/6.8,20)
 
 %%
 
-mean(sqrt([areaCC.Area]/pi)/6.8)
+Requ = (sqrt([areaCC.Area]/pi)/6.8);
+
+%%
+totalArea = sum([areaCC.Area],'all');
+cmArea = cumsum([areaCC.Area]);
+
+totalArea/2;
+
+midCmArea = cmArea(and(cmArea > totalArea/2 - 15000,cmArea < totalArea/2 + 15000 ));
+Requ((cmArea == midCmArea))
+
+%%
+plot(cmArea);hold on
