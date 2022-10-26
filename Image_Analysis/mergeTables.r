53b30eb69b573aa4cc1de912b346a3a1eb9a8ae5
gsiraji <gessiraji@brandeis.edu>
2022-10-26 12:10:37 -0400

    removed some irrelevant files
    + updated the r script for merging tables

--- Image_Analysis/mergeTables.r
+++ Image_Analysis/mergeTables.r
@@ -2,30 +2,41 @@ library(readxl)
 library(tidyverse)
 library(writexl)
 
+#### set working directory ####
+
 setwd("/Users/tfai/Documents/GitHub/Minimal-Lung-Model/Image_Analysis")
 
-table2 <- read_excel("tissue_image_stats.xlsx")
-table1 <- read_excel("image_data_complete.xlsx")
+#### get the image data table and look-up table ####
+image_data_table <- read_excel("tissue_image_data_oldMLI.xlsx")
 
-#### store look-up table ####
-lookupTable <- read_excel("tissue_im_test.xlsx", sheet = "MLI_new", range="A1:E87")
+# store look-up table #
+lookupTable <- read_excel("tissue_im_test.xlsx", sheet = "MLI_old")
 
 
-#### read table ####
-
+#### rearrange, relabel, and organize the look-up table ####
 show(lookupTable)
-select(lookupTable,1,5,2,3,4)
+lookupTable <- select(lookupTable,5,3,1,4,6,7,2)
 colnames(lookupTable)[1] <- "slide"
 colnames(lookupTable)[3] <- "ID"
 colnames(lookupTable)[5] <- "MLI (um)"
+colnames(lookupTable)[7] <- "day"
 
+#### remove NA rows ####
 lookupTable <- lookupTable[-c(48:nrow(lookupTable)),]
-# lookupTable2[42,1] <- 39
-table1 = subset(tissue_image_data, select = -c(11:14) )
-# - ####
+
+#### remove NA rows and order by day and slide ####
+orderedDataTable<-image_data_table[order(image_data_table['day'],
+                  image_data_table['slide'],na.last = NA),]
+
+# remove empty columns ####
+orderedDataTable = subset(orderedDataTable, select = -c(22:25) )
+
+# separate into 2 tables, D0 and D7 ####
+orderedDataTable = subset(orderedDataTable, select = -c(22:25) )
+
 
 #### completeTable <- tissue_image_data[lookupTable, ] ####
-completeTable <- merge(table1, lookupTable, on = c("slide"), all= TRUE)
+completeTable <- merge(table1, lookupTable, on = c("slide","day"), all= TRUE)
 completeTable <- merge(table1,table2,by.x=c("slide","subslide"),by.y=c("c","b"))
 completeTable <- merge(table1, table2, on = c("slide","subslide"))
 #completeTable["AN Tx"] <- completeTable["AN Tx"].tissue_image_data %?% completeTable["AN Tx"].lookupTable2
