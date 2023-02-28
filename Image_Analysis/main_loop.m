dc7cfd57e15ae70d293858ec6994efc6184691f4
gsiraji <gessiraji@brandeis.edu>
2023-02-28 13:58:44 -0500

    added comments to main_loop.m

--- Image_Analysis/main_loop.m
+++ Image_Analysis/main_loop.m
@@ -1,37 +1,64 @@
-% find the first empty row in the Excel sheet
-sheetRowNum = 207;% findEmptyRow('tissue_image_data.xlsx');
-% addpath("/Users/tfai/Documents/research_misc/wimb2022/images/MLI_old_tiff/MLI_old_D0")
+% this is the main loop to run 4 different types of analysis
+% imageProcess(),calculateAreas(), and lumenCorrection() all
+% process images with titles of the from slideVec-slideIdVec.tif
+% or binary images of the format slideVec-slideIdVec.mat
+% ratloop() processes data of the structure provided by Smith's group
+
+% % %
+
+% set the row number in the spreadsheet used for writing the data
+sheetRowNum = 207;
+%% set the necessary variables for processing
+
+% set the image ID numbers corresponding to
+%   1 unique rat
+slideVec = 1:24;
+
+% the images are cropped into up to 10
+%   smaller images. Select a subset of 
+%   these images
+slideIdVec = 1:10;
+
+
+
+
 %% Main Loop
 
-% ratLoop(dat0,0,34)
 
-
-
-slideVec = 1:24;
-slideIdVec = 1:10;
 
 % N used to calculate the area of...
 % the widest tissue sections
 N = 10;
 
+% doFilter = 1 applies entropy filtration to tif files
 doFilter = 0;
+
+% set the age of the rats
 day = 7;
+
+% writeTable = 1 writes the data into the spreadsheet
 writeT = 1;
-% imageProcess(slideVec, slideIdVec, day,...
-%     writeT, doFilter, sheetRowNum, N)
 
-% calculateAreas(slideVec, slideIdVec, day,...
-%     writeT, sheetRowNum)
+%  choose what type of analysis you would like to do
+pickTheAnalysis = 'imageProcess';
 
-lumenCorrection(slideVec,slideIdVec,day,writeT,sheetRowNum)
+switch pickTheAnalysis
 
-%%
-% T = readtable("image_data_complete_oldMLI.xlsx");
-% lengths = T{:,6};
+    % process tif or binary images
+    case 'imageProcess'
+    imageProcess(slideVec, slideIdVec, day,...
+        writeT, doFilter, sheetRowNum, N)
+    
+    % calculate the results of the pruning algorithm
+    case 'calculateAreas'
+    calculateAreas(slideVec, slideIdVec, day,...
+        writeT, sheetRowNum)
+    
+    % find the percentage of lumens that get cropped in each image
+    case 'lumenCorrection'
+    lumenCorrection(slideVec,slideIdVec,day,writeT,sheetRowNum);
 
-%% Functions
-
-function rowIndx = findEmptyRow(fileName)
-tempT = readtable(fileName);
-rowIndx = size(tempT,1);
+    % extract the biomechanical data from the ventilation study
+    case 'ratLoop'
+        ratLoop(dat0,0,34)
 end
